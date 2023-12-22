bede550f3d8862dc7b939bdee1dba654a4e6074b
PhamDuong113 <129020048+PhamDuong113@users.noreply.github.com>
2023-12-22 14:34:50 +0700

    import base client project

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import Button from '@mui/material/Button';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+         <Button variant="contained">Hello world</Button>
+    </>
+  )
+}
+
+export default App
